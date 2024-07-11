56c2679e942ff5664f9c699739e6f4861e828037
yanivabir <yanivabir@gmail.com>
2024-07-11 15:42:43 +0100

    data export and whitelist functions

--- whitelist.jl
+++ whitelist.jl
@@ -15,15 +15,40 @@ begin
 	using CairoMakie, Random, DataFrames, Distributions, Printf, PlutoUI, StatsBase, JSON, CSV, HTTP
 end
 
-# ╔═╡ 0c7b2544-cc07-4219-b262-2af9dae2e320
-function read_REDCap()
-
+# ╔═╡ b2f5b51e-360d-4a9c-bdb4-fd96b82c2919
+begin
 	# Define REDCap API URL and token
 	api_url = "https://redcap.slms.ucl.ac.uk/api/"
 	api_token = "44F85D7A59D210F27C5233D8B39849D9"
+end
 
-	# Create the payload for the POST request
-	payload = Dict(
+# ╔═╡ 20590ede-3ea0-4596-a04f-412c84a93311
+function get_REDCap_file(
+	record_id::Int64
+)
+	# Create the payload for getting the file
+	file_payload = Dict(
+		"token" => api_token,
+	    "content" => "file",
+		"action" => "export",
+		"record" => "13",
+		"field" => "other_data",
+		"returnFormat" => "json"
+	)
+
+	# Make the POST request to the REDCap API
+	file = HTTP.post(api_url, body=HTTP.Form(file_payload), verbose = true)
+
+	# Parse
+	return JSON.parse(String(file.body))
+end
+
+# ╔═╡ 0c7b2544-cc07-4219-b262-2af9dae2e320
+function get_REDCap_data()
+
+	# Get the records --------
+	# Create the payload for getting the record details
+	rec_payload = Dict(
 		"token" => api_token,
 	    "content" => "record",
 	    "action" => "export",
@@ -38,20 +63,53 @@ function read_REDCap()
 	    "returnFormat" => "json"
 	)
 
-	HTTP.Form(payload)
 	# Make the POST request to the REDCap API
-	response = HTTP.post(api_url, body=HTTP.Form(payload))
-	
-	# Parse the JSON response
-	data = JSON.parse(String(response.body))
+	record = HTTP.post(api_url, body=HTTP.Form(rec_payload), verbose = true)
 
-	return data
+	# Parse the JSON response
+	record = JSON.parse(String(record.body))
+
+	# Get the files
+	jspsych_data = []
+	for r in record
+		if r["other_data"] == "file"
+			push!(jspsych_data, get_REDCap_file(parse(Int64, r["record_id"])))
+		end
+	end
+
+	# Convert to DataFrame
+	jspsych_data = reduce(vcat, jspsych_data)
+
+	jspsych_data = vcat(
+		[DataFrame(d) for d in jspsych_data]...,
+		cols=:union
+	)
+	
+	return jspsych_data
 end
 
-# ╔═╡ 47b7379d-c0f7-4276-8904-8a17e01ee9e9
-data = read_REDCap()
+# ╔═╡ 1588fe86-cf3d-496a-8199-3abb2c36232a
+data = get_REDCap_data()
+
+# ╔═╡ d5369cbb-696b-4caf-8986-5ea4f983970a
+function whitelist(data)
+	whitelist = combine(groupby(data, [:prolific_pid, :session]),
+		:trialphase => (x -> "experiment_end_message" in x) => :finished,
+		:outcomes => 
+			(x -> filter(y -> !ismissing(y), unique(data.outcomes))[1]) => :bonus,
+		:trial_type => (x -> sum(x .== "PLT")) => :n_trial_PLT,
+		:block => (x -> filter(y -> typeof(y) == Int64, x) |> unique |> length) => :n_blocks_PLT
+	)
+end
+
+# ╔═╡ e81fd624-ad5a-4dda-80d6-b55681a177d7
+whitelist(data)
 
 # ╔═╡ Cell order:
 # ╠═053887ce-3f7d-11ef-0ab9-53a5a4738d90
+# ╠═b2f5b51e-360d-4a9c-bdb4-fd96b82c2919
+# ╠═20590ede-3ea0-4596-a04f-412c84a93311
 # ╠═0c7b2544-cc07-4219-b262-2af9dae2e320
-# ╠═47b7379d-c0f7-4276-8904-8a17e01ee9e9
+# ╠═1588fe86-cf3d-496a-8199-3abb2c36232a
+# ╠═d5369cbb-696b-4caf-8986-5ea4f983970a
+# ╠═e81fd624-ad5a-4dda-80d6-b55681a177d7
