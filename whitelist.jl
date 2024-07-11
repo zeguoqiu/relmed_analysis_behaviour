ef8ff607caf04d7d6345856bf7e799dbf4241adb
yanivabir <yanivabir@gmail.com>
2024-07-11 21:38:30 +0100

    fix download, prepare whitelist

--- whitelist.jl
+++ whitelist.jl
@@ -24,14 +24,14 @@ end
 
 # ╔═╡ 20590ede-3ea0-4596-a04f-412c84a93311
 function get_REDCap_file(
-	record_id::Int64
+	record_id::String
 )
 	# Create the payload for getting the file
 	file_payload = Dict(
 		"token" => api_token,
 	    "content" => "file",
 		"action" => "export",
-		"record" => "13",
+		"record" => record_id,
 		"field" => "other_data",
 		"returnFormat" => "json"
 	)
@@ -73,7 +73,7 @@ function get_REDCap_data()
 	jspsych_data = []
 	for r in record
 		if r["other_data"] == "file"
-			push!(jspsych_data, get_REDCap_file(parse(Int64, r["record_id"])))
+			push!(jspsych_data, get_REDCap_file(r["record_id"]))
 		end
 	end
 
@@ -88,21 +88,33 @@ function get_REDCap_data()
 	return jspsych_data
 end
 
-# ╔═╡ 1588fe86-cf3d-496a-8199-3abb2c36232a
-data = get_REDCap_data()
+# ╔═╡ 5fa35526-da89-4f36-a666-350f68164740
+remove_testing!(data::DataFrame) = filter!(x -> !occursin(r"yaniv|tore", x.prolific_pid), data)
 
 # ╔═╡ d5369cbb-696b-4caf-8986-5ea4f983970a
 function whitelist(data)
-	whitelist = combine(groupby(data, [:prolific_pid, :session]),
+	whitelist = combine(groupby(data, [:prolific_pid, :session, :condition]),
 		:trialphase => (x -> "experiment_end_message" in x) => :finished,
+		:trialphase => (x -> "kick-out" in x) => :kick_out,
 		:outcomes => 
-			(x -> filter(y -> !ismissing(y), unique(data.outcomes))[1]) => :bonus,
-		:trial_type => (x -> sum(x .== "PLT")) => :n_trial_PLT,
-		:block => (x -> filter(y -> typeof(y) == Int64, x) |> unique |> length) => :n_blocks_PLT
+			(x -> filter(y -> !ismissing(y), unique(x)) |> z -> length(z) > 0 ? z[1] : missing) => :bonus,
+		[:trial_type, :block] => ((t, b) -> sum((t .== "PLT") .& (typeof.(b) .== Int64))) => :n_trial_PLT,
+		:block => (x -> filter(y -> typeof(y) == Int64, x) |> unique |> length) => :n_blocks_PLT,
+		:n_warnings => maximum => :n_warnings
 	)
+
+	sort!(whitelist, :n_blocks_PLT, rev = true)
+
+	return whitelist
 end
 
+# ╔═╡ 0127aa42-9506-4c6f-8f43-dd4308b3195c
+data = get_REDCap_data()
+
 # ╔═╡ e81fd624-ad5a-4dda-80d6-b55681a177d7
+remove_testing!(data)
+
+# ╔═╡ 86942e04-639e-428c-bc50-4b532daf1ec6
 whitelist(data)
 
 # ╔═╡ Cell order:
@@ -110,6 +122,8 @@ whitelist(data)
 # ╠═b2f5b51e-360d-4a9c-bdb4-fd96b82c2919
 # ╠═20590ede-3ea0-4596-a04f-412c84a93311
 # ╠═0c7b2544-cc07-4219-b262-2af9dae2e320
-# ╠═1588fe86-cf3d-496a-8199-3abb2c36232a
+# ╠═5fa35526-da89-4f36-a666-350f68164740
 # ╠═d5369cbb-696b-4caf-8986-5ea4f983970a
+# ╠═0127aa42-9506-4c6f-8f43-dd4308b3195c
 # ╠═e81fd624-ad5a-4dda-80d6-b55681a177d7
+# ╠═86942e04-639e-428c-bc50-4b532daf1ec6
