ea9a11051a70b4ee24bcf09d34904461aa5d488f
yanivabir <yanivabir@gmail.com>
2024-07-14 20:49:28 +0100

    set theme and laod data

--- CP_MN.jl
+++ CP_MN.jl
@@ -19,8 +19,33 @@ begin
 end
 
 # ╔═╡ bdeadcc4-1a5f-4c39-a055-e61b3db3f3b1
+begin
+	# Set theme
+	inter_bold = assetpath(pwd() * "/fonts/Inter/Inter-Bold.ttf")
+	
+	th = Theme(
+		font = "Helvetica",
+		fontsize = 16,
+		Axis = (
+			xgridvisible = false,
+			ygridvisible = false,
+			rightspinevisible = false,
+			topspinevisible = false,
+			xticklabelsize = 14,
+			yticklabelsize = 14,
+			spinewidth = 1.5,
+			xtickwidth = 1.5,
+			ytickwidth = 1.5
+		)
+	)
+	set_theme!(th)
+end
 
+# ╔═╡ 963e5f75-00f9-4fcc-90b9-7ecfb7e278f2
+# Load data
+PLT_data = load_PLT_data()
 
 # ╔═╡ Cell order:
 # ╠═e01188c3-ca30-4a7c-9101-987752139a71
 # ╠═bdeadcc4-1a5f-4c39-a055-e61b3db3f3b1
+# ╠═963e5f75-00f9-4fcc-90b9-7ecfb7e278f2
