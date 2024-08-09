a21fa48b48bd98191ac1f82060fa100372002d60
yanivabir <yanivabir@gmail.com>
2024-08-09 22:00:43 +0100

    sum_SBC_draws

---
+++ sample_utils.jl
@@ -0,0 +1,42 @@
+# Functions for working with draws from models
+
+# Summarize prior predictive draws relative to true value
+function sum_SBC_draws(
+	draws;
+	params::Vector{Symbol}, # Parameters to sum
+	true_values::Vector{Float64},
+	prior_var::Vector{Float64} = repeat([1.], length(params)) # Variance of the prior for computing posterior contraction
+	)
+
+	if length(size(draws)) > 2 # For MCMC Chains object
+		tdraws = DataFrame(Array(draws), names(draws, :parameters))
+	else
+		tdraws = draws
+	end
+
+	sums = []
+	for (p, t, pv) in zip(params, true_values, prior_var)
+
+		v = tdraws[!, p]
+		v_s = v .- t
+
+		push!(sums,
+			(;
+				Symbol("$(p)_m") => median(v), # Median posterior
+				Symbol("$(p)_lb") => lb(v), # Posterior 25th percentile
+				Symbol("$(p)_ub") => ub(v), # Posterior 75th percentile
+				Symbol("$(p)_zs") => mean(v_s) / std(v), # Posterior zscore
+				Symbol("$(p)_sm") => median(v_s), # Median error
+				Symbol("$(p)_slb") => lb(v_s), # Error 25th percentile
+				Symbol("$(p)_sub") => ub(v_s), # Error 75th percentile
+				Symbol("$(p)_cntrct") => 1 - var(v) / pv, # Posterior contraction, after Schad et al. 2021
+				Symbol("true_$p") => t
+			)		
+		)
+
+	end
+
+	# Return as one NamedTuple
+	return reduce((x, y) -> merge(x, y), sums)
+
+end
