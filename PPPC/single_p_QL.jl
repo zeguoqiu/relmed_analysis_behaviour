93eb81ec5181edbc60c4d50ea476b8a45d63896c
yanivabir <yanivabir@gmail.com>
2024-08-11 14:31:55 +0100

    prior and posterior predictive suite for single_p_QL.jl

---
+++ PPPC/single_p_QL.jl
@@ -0,0 +1,191 @@
+### A Pluto.jl notebook ###
+# v0.19.43
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ fb94ad20-57e0-11ef-2dae-b16d3d00e329
+begin
+	cd("/home/jovyan")
+	import Pkg
+	
+	# activate the shared project environment
+    Pkg.activate("$(pwd())/relmed_environment")
+    # instantiate, i.e. make sure that all packages are downloaded
+    Pkg.instantiate
+	using CairoMakie, Random, DataFrames, Distributions, StatsBase,
+		ForwardDiff, LinearAlgebra, JLD2, FileIO, CSV, Dates, JSON, RCall, Turing, ParetoSmooth, MCMCDiagnosticTools, Printf
+	using LogExpFunctions: logistic, logit
+
+	include("$(pwd())/sample_utils.jl")
+	include("$(pwd())/plotting_utils.jl")
+	include("$(pwd())/single_p_QL.jl")
+end
+
+# ╔═╡ 261d0d08-10b9-4111-9fc8-bb84e6b4cef5
+begin
+	# Set theme
+	inter_bold = assetpath(pwd() * "/fonts/Inter/Inter-Bold.ttf")
+	
+	th = Theme(
+		font = "Helvetica",
+		fontsize = 16,
+		Axis = (
+			xgridvisible = false,
+			ygridvisible = false,
+			rightspinevisible = false,
+			topspinevisible = false,
+			xticklabelsize = 14,
+			yticklabelsize = 14,
+			spinewidth = 1.5,
+			xtickwidth = 1.5,
+			ytickwidth = 1.5
+		)
+	)
+	set_theme!(th)
+end
+
+# ╔═╡ fa79c576-d4c9-4c42-b5df-66d886e8abe4
+# Sample datasets from prior
+begin
+	prior_sample = let
+		# Load sequence from file
+		task = DataFrame(CSV.File("data/PLT_task_structure_00.csv"))
+	
+		# Renumber block
+		task.block = task.block .+ (task.session .- 1) * maximum(task.block)
+	
+		# Arrange feedback by optimal / suboptimal
+		task.feedback_optimal = 
+			ifelse.(task.optimal_A .== 1, task.feedback_A, task.feedback_B)
+	
+		task.feedback_suboptimal = 
+			ifelse.(task.optimal_A .== 0, task.feedback_A, task.feedback_B)
+	
+	
+		# Arrange outcomes such as second column is optimal
+		outcomes = hcat(
+			task.feedback_suboptimal,
+			task.feedback_optimal,
+		)
+	
+		# Initial value for Q values
+		aao = mean([mean([0.01, mean([0.5, 1.])]), mean([1., mean([0.5, 0.01])])])
+	
+		prior_sample = simulate_single_p_QL(
+			100;
+			block = task.block,
+			valence = unique(task[!, [:block, :valence]]).valence,
+			outcomes = outcomes,
+			initV = fill(aao, 1, 2),
+			random_seed = 0
+		)
+	
+	
+		leftjoin(prior_sample, 
+			task[!, [:block, :trial, :feedback_optimal, :feedback_suboptimal]],
+			on = [:block, :trial]
+		)
+	
+	end
+
+	describe(prior_sample)
+end
+
+# ╔═╡ d7b60f28-09b1-42c0-8c95-0213590d8c5c
+# Plot prior preditctive accuracy curve
+let
+	df = rename(prior_sample, 
+		:Q_optimal => :EV_A,
+		:Q_suboptimal => :EV_B
+	)
+
+	df[!, :group] .= 1
+	
+	f = Figure(size = (700, 1000))
+
+	g_all = f[1,1] = GridLayout()
+	
+	plot_sim_q_value_acc!(
+		g_all,
+		df;
+		plw = 1,
+		legend = false,
+		acc_error_band = "PI"
+	)
+
+	g_reward = f[2,1] = GridLayout()
+	
+	plot_sim_q_value_acc!(
+		g_reward,
+		filter(x -> x.valence > 0, df);
+		plw = 1,
+		legend = false,
+		acc_error_band = "PI"
+	)
+
+	g_reward = f[3,1] = GridLayout()
+	
+	plot_sim_q_value_acc!(
+		g_reward,
+		filter(x -> x.valence < 0, df);
+		plw = 1,
+		legend = false,
+		acc_error_band = "PI"
+	)
+
+	f
+end
+
+# ╔═╡ 9477b295-ada5-46cf-b2e3-2c1303873081
+# Fit and plot single participant
+begin
+	fit = let
+		# Initial value for Q values
+		aao = mean([mean([0.01, mean([0.5, 1.])]), mean([1., mean([0.5, 0.01])])])
+		
+		fit = posterior_sample_single_p_QL(
+			filter(x -> x.PID == 1, prior_sample); 
+			initV = aao,
+			random_seed = 0
+		)
+	
+		fit
+	
+	end
+
+	plot_posteriors([fit],
+		["a", "ρ"];
+		true_values = [α2a(prior_sample[1, :α]), prior_sample[1, :ρ]]
+	)	
+end
+
+# ╔═╡ 2afaba84-49b6-4770-a3bb-6e8e4c8be4ba
+sbc = let
+	draws_sum_file = "saved_models/sbc_single_p_QL.jld2"
+	if isfile(draws_sum_file)
+		JLD2.@load draws_sum_file sbc
+	else
+		sbc = SBC_single_p_QL(
+			prior_sample;
+			initV = mean([mean([0.01, mean([0.5, 1.])]), mean([1., mean([0.5, 0.01])])]),
+			random_seed = 0
+		) |> DataFrame
+
+		JLD2.@save draws_sum_file sbc
+	end
+
+	sbc
+end
+
+# ╔═╡ 50d88c5c-4a3f-4ded-81cf-600eddb3bbf9
+plot_SBC(sbc, show_n = [1], params = ["a", "ρ"])
+
+# ╔═╡ Cell order:
+# ╠═fb94ad20-57e0-11ef-2dae-b16d3d00e329
+# ╠═261d0d08-10b9-4111-9fc8-bb84e6b4cef5
+# ╠═fa79c576-d4c9-4c42-b5df-66d886e8abe4
+# ╠═d7b60f28-09b1-42c0-8c95-0213590d8c5c
+# ╠═9477b295-ada5-46cf-b2e3-2c1303873081
+# ╠═2afaba84-49b6-4770-a3bb-6e8e4c8be4ba
+# ╠═50d88c5c-4a3f-4ded-81cf-600eddb3bbf9
