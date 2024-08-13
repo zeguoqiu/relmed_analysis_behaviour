91b12c3e27a9ed55e90c72ea0e5cbe84fce2ca66
yanivabir <yanivabir@gmail.com>
2024-08-13 12:07:33 +0100

    use function to sample from prior

--- PPPC/independent_group_QL.jl
+++ PPPC/independent_group_QL.jl
@@ -23,8 +23,7 @@ begin
 end
 
 # ╔═╡ ba3495da-93cc-4002-a2a5-99589a819a94
-begin
-	n_participants = 10
+prior_sample = let n_participants = 10
 	
 	# Load sequence from file
 	task = DataFrame(CSV.File("data/PLT_task_structure_00.csv"))
@@ -46,72 +45,25 @@ begin
 		task.feedback_optimal,
 	)
 
-	# Initial value for Q values
-	aao = mean([mean([0.01, mean([0.5, 1.])]), mean([1., mean([0.5, 0.01])])])
-
+	# Arrange values for simulation
 	block = fill(task.block, n_participants)
 	valence = fill(unique(task[!, [:block, :valence]]).valence, n_participants)
 	outcomes = fill(outcomes, n_participants)
 
-	prior_model = independent_group_QL(
+	# Initial value for Q values
+	aao = mean([mean([0.01, mean([0.5, 1.])]), mean([1., mean([0.5, 0.01])])])
+
+	simulate_independent_group_QL(
+		1;
 		block = block,
 		valence = valence,
-		choice = [fill(missing, length(block[p])) for p in eachindex(block)],
 		outcomes = outcomes,
-		initV = fill(aao, 1, 2)
-	)
-
-
-	# Draw parameters and simulate choice
-	prior_sample = sample(
-		prior_model,
-		Prior(),
-		9
+		initV = fill(aao, 1, 2),
+		random_seed = 0
 	)
 
 end
-
-# ╔═╡ 4b1235b9-5fa8-4323-acc5-0fcb57a0fc25
-begin
-    ρ = single_layer_chain_to_vector(prior_sample, "ρ")
-	a = single_layer_chain_to_vector(prior_sample, "a")
-end
-
-# ╔═╡ 79aaa431-8d0b-4fad-87c1-9d2cc9040641
-	# Compute Q values
-	generated_quantities(prior_model, prior_sample) |> vec
-
-
-# ╔═╡ a383c231-79f1-4cde-9e09-8ff46e0b835c
-Qs
-
-# ╔═╡ c14b4f42-e328-4ba4-85a7-be65404f1cba
-begin
-	param = "choice"
-	matching_columns = filter(x -> occursin(Regex("$(param)\\[\\d+\\]\\[\\d+\\]"), x), string.(names(prior_sample, :parameters)))
-    wide_df = prior_sample[:, matching_columns, 1] |> DataFrame
-
-	select!(wide_df, Not(:chain))
-	long_df = stack(wide_df, matching_columns, :iteration, value_name = :choice)
-
-	long_df.PID = (x -> parse(Int64, match(Regex("$(param)\\[(\\d+)\\]\\[(\\d+)\\]"), x)[1])).(long_df.variable)
-
-	long_df.trial = (x -> parse(Int64, 
-        match(Regex("$(param)\\[(\\d+)\\]\\[(\\d+)\\]"), x)[2])).(long_df.variable)
-
-	select!(long_df, [:iteration, :PID, :trial, :choice])
-	
-	long_df
-end
-
-# ╔═╡ 96102f32-1bdc-4d11-992c-f003a24fac6a
-rand(BernoulliLogit()) + 0
 
 # ╔═╡ Cell order:
 # ╠═fe776a2c-589a-11ef-0331-472f2769c12f
 # ╠═ba3495da-93cc-4002-a2a5-99589a819a94
-# ╠═4b1235b9-5fa8-4323-acc5-0fcb57a0fc25
-# ╠═79aaa431-8d0b-4fad-87c1-9d2cc9040641
-# ╠═a383c231-79f1-4cde-9e09-8ff46e0b835c
-# ╠═c14b4f42-e328-4ba4-85a7-be65404f1cba
-# ╠═96102f32-1bdc-4d11-992c-f003a24fac6a
