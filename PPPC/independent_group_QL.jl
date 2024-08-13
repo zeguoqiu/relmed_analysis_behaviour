aa438788030a76c788ae5274ac9e689496375d97
yanivabir <yanivabir@gmail.com>
2024-08-13 20:11:18 +0100

    fit from prior

--- PPPC/independent_group_QL.jl
+++ PPPC/independent_group_QL.jl
@@ -90,66 +90,86 @@ end
 
 # ╔═╡ 76bf001d-c20f-4c23-bc35-a483679a96e0
 # Plot prior preditctive accuracy curve
+# let
+# 	# Unpack prior sample into DataFrame
+# 	df = DataFrame(
+# 		:PID => vcat([
+# 			fill(i, length(b)) for (i, b) in enumerate(prior_sample[:block])]...),
+# 		:block => vcat(prior_sample[:block]...),
+# 		:choice => vcat(prior_sample[:choice]...),
+# 		:EV_A => vcat([q[:, 2] for q in prior_sample[:Qs]]...),
+# 		:EV_B => vcat([q[:, 1] for q in prior_sample[:Qs]]...),
+# 		:ρ => vcat([
+# 			fill(prior_sample[:ρ][i], length(b)) for (i, b) in enumerate(prior_sample[:block])]...)
+# 	)
+
+# 	# Add needed variables that are more difficult to arrange
+# 	DataFrames.transform!(
+# 		groupby(df, [:PID, :block]),
+# 		:block => (x -> 1:length(x)) => :trial,
+# 		[:PID, :block] => ByRow((p, b) -> prior_sample[:valence][p][b]) => :valence
+# 	)
+
+# 	df[!, :group] .= 1
+
+# 	f = Figure(size = (700, 1000))
+
+# 	g_all = f[1,1] = GridLayout()
+	
+# 	plot_sim_q_value_acc!(
+# 		g_all,
+# 		df;
+# 		plw = 1,
+# 		legend = false,
+# 		acc_error_band = "PI"
+# 	)
+
+# 	g_reward = f[2,1] = GridLayout()
+	
+# 	plot_sim_q_value_acc!(
+# 		g_reward,
+# 		filter(x -> x.valence > 0, df);
+# 		plw = 1,
+# 		legend = false,
+# 		acc_error_band = "PI"
+# 	)
+
+# 	g_reward = f[3,1] = GridLayout()
+	
+# 	plot_sim_q_value_acc!(
+# 		g_reward,
+# 		filter(x -> x.valence < 0, df);
+# 		plw = 1,
+# 		legend = false,
+# 		acc_error_band = "PI"
+# 	)
+
+# 	f
+
+# end
+
+# ╔═╡ 5f3fea09-f7a5-4f2c-89f3-091c83f1abe8
 let
-	# Unpack prior sample into DataFrame
-	df = DataFrame(
-		:PID => vcat([
-			fill(i, length(b)) for (i, b) in enumerate(prior_sample[:block])]...),
-		:block => vcat(prior_sample[:block]...),
-		:choice => vcat(prior_sample[:choice]...),
-		:EV_A => vcat([q[:, 2] for q in prior_sample[:Qs]]...),
-		:EV_B => vcat([q[:, 1] for q in prior_sample[:Qs]]...),
-		:ρ => vcat([
-			fill(prior_sample[:ρ][i], length(b)) for (i, b) in enumerate(prior_sample[:block])]...)
+
+	model = independent_group_QL_threaded(;
+		block = prior_sample[:block],
+		valence = prior_sample[:valence],
+		choice = prior_sample[:choice],
+		outcomes = prior_sample[:outcomes],
+		initV = prior_sample[:initV]
 	)
 
-	# Add needed variables that are more difficult to arrange
-	DataFrames.transform!(
-		groupby(df, [:PID, :block]),
-		:block => (x -> 1:length(x)) => :trial,
-		[:PID, :block] => ByRow((p, b) -> prior_sample[:valence][p][b]) => :valence
-	)
-
-	df[!, :group] .= 1
-
-	f = Figure(size = (700, 1000))
-
-	g_all = f[1,1] = GridLayout()
-	
-	plot_sim_q_value_acc!(
-		g_all,
-		df;
-		plw = 1,
-		legend = false,
-		acc_error_band = "PI"
-	)
-
-	g_reward = f[2,1] = GridLayout()
-	
-	plot_sim_q_value_acc!(
-		g_reward,
-		filter(x -> x.valence > 0, df);
-		plw = 1,
-		legend = false,
-		acc_error_band = "PI"
-	)
-
-	g_reward = f[3,1] = GridLayout()
-	
-	plot_sim_q_value_acc!(
-		g_reward,
-		filter(x -> x.valence < 0, df);
-		plw = 1,
-		legend = false,
-		acc_error_band = "PI"
-	)
-
-	f
+	logjoint(model, (a = fill(1., 100), ρ = fill(2., 100)))
 
 end
+
+# ╔═╡ 32b338c0-3a04-4759-bff0-8c04a3201f56
+Threads.nthreads()
 
 # ╔═╡ Cell order:
 # ╠═fe776a2c-589a-11ef-0331-472f2769c12f
 # ╠═998eed23-55e1-4bcd-b995-21ca82577454
 # ╠═ba3495da-93cc-4002-a2a5-99589a819a94
 # ╠═76bf001d-c20f-4c23-bc35-a483679a96e0
+# ╠═5f3fea09-f7a5-4f2c-89f3-091c83f1abe8
+# ╠═32b338c0-3a04-4759-bff0-8c04a3201f56
