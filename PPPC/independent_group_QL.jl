69511bd99ce3f2d80ac5b1a70bf430ffc7a75ada
yanivabir <yanivabir@gmail.com>
2024-08-13 13:05:34 +0100

    prior predictive

--- PPPC/independent_group_QL.jl
+++ PPPC/independent_group_QL.jl
@@ -22,8 +22,32 @@ begin
 	include("$(pwd())/independent_group_QL.jl")
 end
 
+# ╔═╡ 998eed23-55e1-4bcd-b995-21ca82577454
+begin
+	# Set theme
+	inter_bold = assetpath(pwd() * "/fonts/Inter/Inter-Bold.ttf")
+	
+	th = Theme(
+		font = "Helvetica",
+		fontsize = 16,
+		Axis = (
+			xgridvisible = false,
+			ygridvisible = false,
+			rightspinevisible = false,
+			topspinevisible = false,
+			xticklabelsize = 14,
+			yticklabelsize = 14,
+			spinewidth = 1.5,
+			xtickwidth = 1.5,
+			ytickwidth = 1.5
+		)
+	)
+	set_theme!(th)
+end
+
 # ╔═╡ ba3495da-93cc-4002-a2a5-99589a819a94
-prior_sample = let n_participants = 10
+# Simulate one dataset
+prior_sample = let n_participants = 100
 	
 	# Load sequence from file
 	task = DataFrame(CSV.File("data/PLT_task_structure_00.csv"))
@@ -64,6 +88,68 @@ prior_sample = let n_participants = 10
 
 end
 
+# ╔═╡ 76bf001d-c20f-4c23-bc35-a483679a96e0
+# Plot prior preditctive accuracy curve
+let
+	# Unpack prior sample into DataFrame
+	df = DataFrame(
+		:PID => vcat([
+			fill(i, length(b)) for (i, b) in enumerate(prior_sample[:block])]...),
+		:block => vcat(prior_sample[:block]...),
+		:choice => vcat(prior_sample[:choice]...),
+		:EV_A => vcat([q[:, 2] for q in prior_sample[:Qs]]...),
+		:EV_B => vcat([q[:, 1] for q in prior_sample[:Qs]]...),
+		:ρ => vcat([
+			fill(prior_sample[:ρ][i], length(b)) for (i, b) in enumerate(prior_sample[:block])]...)
+	)
+
+	# Add needed variables that are more difficult to arrange
+	DataFrames.transform!(
+		groupby(df, [:PID, :block]),
+		:block => (x -> 1:length(x)) => :trial,
+		[:PID, :block] => ByRow((p, b) -> prior_sample[:valence][p][b]) => :valence
+	)
+
+	df[!, :group] .= 1
+
+	f = Figure(size = (700, 1000))
+
+	g_all = f[1,1] = GridLayout()
+	
+	plot_sim_q_value_acc!(
+		g_all,
+		df;
+		plw = 1,
+		legend = false,
+		acc_error_band = "PI"
+	)
+
+	g_reward = f[2,1] = GridLayout()
+	
+	plot_sim_q_value_acc!(
+		g_reward,
+		filter(x -> x.valence > 0, df);
+		plw = 1,
+		legend = false,
+		acc_error_band = "PI"
+	)
+
+	g_reward = f[3,1] = GridLayout()
+	
+	plot_sim_q_value_acc!(
+		g_reward,
+		filter(x -> x.valence < 0, df);
+		plw = 1,
+		legend = false,
+		acc_error_band = "PI"
+	)
+
+	f
+
+end
+
 # ╔═╡ Cell order:
 # ╠═fe776a2c-589a-11ef-0331-472f2769c12f
+# ╠═998eed23-55e1-4bcd-b995-21ca82577454
 # ╠═ba3495da-93cc-4002-a2a5-99589a819a94
+# ╠═76bf001d-c20f-4c23-bc35-a483679a96e0
