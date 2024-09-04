2d8bc3d6ff168e64098968d162c0331f3d68164c
yanivabir <yanivabir@gmail.com>
2024-09-04 22:04:10 +0100

    plot likelihood for more conditions

--- PPPC/validate_single_p_QL.jl
+++ PPPC/validate_single_p_QL.jl
@@ -53,41 +53,89 @@ end
 # Initial value for Q values
 aao = mean([mean([0.01, mean([0.5, 1.])]), mean([1., mean([0.5, 0.01])])])
 
-# ╔═╡ 6831726a-e050-4a13-afbc-bb1239ad2ab6
-# Sample single dataset from prior
-function simulate_participant(;
-	condition::String,
-	ρ::Float64,
-	a::Float64,
-	repeats::Int64 = 1
-	)	
-		# Load sequence from file
-		task = task_vars_for_condition("110")
+# ╔═╡ b018bc38-bde2-4bff-bbc8-79e4dd81938e
+begin
+	# Sample single dataset from prior
+	function simulate_participant(;
+		condition::String,
+		ρ::Float64,
+		a::Float64,
+		repeats::Int64 = 1
+		)	
+			# Load sequence from file
+			task = task_vars_for_condition("110")
+			
+			prior_sample = simulate_single_p_QL(
+				repeats;
+				block = task.block,
+				valence = task.valence,
+				outcomes = task.outcomes,
+				initV = fill(aao, 1, 2),
+				random_seed = 0,
+				prior_ρ = Dirac(ρ),
+				prior_a = Dirac(a)
+			)
 		
-		prior_sample = simulate_single_p_QL(
-			repeats;
-			block = task.block,
-			valence = task.valence,
-			outcomes = task.outcomes,
-			initV = fill(aao, 1, 2),
-			random_seed = 0,
-			prior_ρ = Dirac(ρ),
-			prior_a = Dirac(a)
-		)
+		
+			prior_sample = leftjoin(prior_sample, 
+				task.task[!, [:block, :trial, :feedback_optimal, :feedback_suboptimal]],
+				on = [:block, :trial]
+			)
 	
+			# Renumber blocks
+			if repeats > 1
+					prior_sample.block = prior_sample.block .+ 
+						(prior_sample.PID .- 1) .* maximum(prior_sample.block)
+			end
 	
-		prior_sample = leftjoin(prior_sample, 
-			task.task[!, [:block, :trial, :feedback_optimal, :feedback_suboptimal]],
-			on = [:block, :trial]
-		)
+		return prior_sample
+	end
+	
+	# Sample single dataset from prior, draw task randomly
+	function simulate_participant_random_task(;
+		ρ::Float64,
+		a::Float64,
+		n_blocks::Int64 = 48,
+		n_trials::Int64 = 13
+		)	
+			# Create task sequence
+			block = repeat(1:n_blocks, inner = n_trials)
+			valence = ones(Int64, n_blocks)
+			outcomes = hcat(
+				rand(Normal(), n_blocks * n_trials),
+				rand(Normal(0.5, 1.), n_blocks * n_trials)
+			)
+			trial = repeat(1:n_trials, n_blocks)
+			
+			prior_sample = simulate_single_p_QL(
+				1;
+				block = block,
+				valence = valence,
+				outcomes = outcomes,
+				initV = fill(0., 1, 2),
+				random_seed = 0,
+				prior_ρ = Dirac(ρ),
+				prior_a = Dirac(a)
+			)
+		
+			
+			prior_sample.trial = trial
+	
+			prior_sample.feedback_suboptimal = outcomes[:, 1]
+	
+			prior_sample.feedback_optimal = outcomes[:, 2]
+	
+		return prior_sample
+	end
+end
 
-		# Renumber blocks
-		if repeats > 1
-				prior_sample.block = prior_sample.block .+ 
-					(prior_sample.PID .- 1) .* maximum(prior_sample.block)
-		end
+# ╔═╡ 61dec821-a308-48cb-ab41-7124369810f4
+let n_blocks = 48,
+	n_trials = 13
 
-	return prior_sample
+	block = repeat(1:n_blocks, inner = n_trials)
+	valence = ones(Int64, n_blocks * n_trials)
+	length(valence)
 end
 
 # ╔═╡ 14e78184-fa18-4489-89d8-41dfd3044203
@@ -124,14 +172,28 @@ function plot_turing_ll(
 	)
 
 	# Plot loglikelihood
+	ρ = repeat(grid_ρ, inner = length(grid_a))
+	a = repeat(grid_a, outer = length(grid_ρ))
+	ll = [loglikelihood(
+			post_model, 
+			(ρ = ρ, a = a)) for ρ in grid_ρ for a in grid_a]
+	
 	contour!(
 		ax,
-		repeat(grid_ρ, inner = length(grid_a)),
-		repeat(grid_a, outer = length(grid_ρ)),
-		[loglikelihood(
-			post_model, 
-			(ρ = ρ, a = a)) for ρ in grid_ρ for a in grid_a],
+		ρ,
+		a, 
+		ll,
 		levels = 10
+	)
+
+	# Plot MLE
+	scatter!(
+		ax,
+		ρ[argmax(ll)],
+		a[argmax(ll)],
+		marker = :cross,
+		markersize = 8,
+		color = :blue
 	)
 
 	return ax
@@ -190,7 +252,7 @@ function plot_handcrafted_ll(
 )
 	
 	# Set up model
-	ll = (ρ, a) -> single_p_QL_ll(;
+	llf = (ρ, a) -> single_p_QL_ll(;
 		N = nrow(data),
 		n_blocks = maximum(data.block),
 		block = data.block,
@@ -212,14 +274,29 @@ function plot_handcrafted_ll(
 		ylabel = "a"
 	)
 
-	#Plot loglikelihood
+	# Plot loglikelihood
+	ρ = repeat(grid_ρ, inner = length(grid_a))
+	a = repeat(grid_a, outer = length(grid_ρ))
+	ll = [llf(ρ, a) for ρ in grid_ρ for a in grid_a]
+	
 	contour!(
 		ax,
-		repeat(grid_ρ, inner = length(grid_a)),
-		repeat(grid_a, outer = length(grid_ρ)),
-		[ll(ρ, a) for ρ in grid_ρ for a in grid_a],
+		ρ,
+		a,
+		ll,
 		levels = 10
 	)
+
+	# Plot MLE
+	scatter!(
+		ax,
+		ρ[argmax(ll)],
+		a[argmax(ll)],
+		marker = :cross,
+		markersize = 8,
+		color = :blue
+	)
+
 
 	return ax
 
@@ -232,15 +309,23 @@ function simulate_plot_ll!(
 	ρ::Float64,
 	a::Float64,
 	title::String = "",
-	repeats::Int64 = 1
+	repeats::Int64 = 1,
 )
 
-	prior_sample = simulate_participant(;
-		condition = condition,
-		ρ = ρ,
-		a = a,
-		repeats = repeats
-	)
+	if condition == "random"
+		prior_sample = simulate_participant_random_task(;
+			ρ = ρ,
+			a = a,
+			n_blocks = repeats
+		)
+	else
+		prior_sample = simulate_participant(;
+			condition = condition,
+			ρ = ρ,
+			a = a,
+			repeats = repeats
+		)
+	end
 
 	ax = plot_turing_ll(
 		f;
@@ -252,6 +337,36 @@ function simulate_plot_ll!(
 	ax.title = title
 
 	return ax
+end
+
+# ╔═╡ d0cbfe44-5f88-4833-9313-bad04f51342b
+let
+
+	f = Figure(size = (700, 700 / 4))
+
+	for (i, r) in enumerate([1, 5, 10, 20] .* 48)
+		ax = simulate_plot_ll!(
+			f[1, i];
+			condition = "random",
+			ρ = 6.,
+			a = 0.5,
+			repeats = r
+		)
+
+		ax.title = "$(r)"
+
+		scatter!(
+			ax,
+			6.,
+			0.5,
+			marker = :star5,
+			markersize = 15,
+			color = :red
+		)
+	end
+
+	f
+
 end
 
 # ╔═╡ 032842c4-507b-4a58-b860-5a085b93ac47
@@ -268,6 +383,8 @@ let
 			repeats = r
 		)
 
+		ax.title = "$(r*48)"
+
 		scatter!(
 			ax,
 			6.,
@@ -282,9 +399,36 @@ let
 
 end
 
+# ╔═╡ 2d44b2a8-218b-49dd-ac6d-f4ac45e2c216
+let
+
+	f = Figure(size = (700, 700))
+
+	for (i, ρ) in enumerate(range(0.5, 10., length = 4))
+		for (j, a) in enumerate(range(-2., 2., length = 4))
+			ax = simulate_plot_ll!(
+				f[j, i];
+				condition = "random",
+				ρ = ρ,
+				a = a
+			)
+
+			scatter!(
+				ax,
+				ρ,
+				a,
+				marker = :star5,
+				markersize = 15,
+				color = :red
+			)
+		end
+	end
+
+	f
+
+end
+
 # ╔═╡ e73d8171-98df-4955-a627-20b40a9a2a5c
-# ╠═╡ disabled = true
-#=╠═╡
 let
 
 	f = Figure(size = (700, 700))
@@ -312,7 +456,6 @@ let
 	f
 
 end
-  ╠═╡ =#
 
 # ╔═╡ a3c71e4e-03ca-41fd-8a1d-6cedd0da068e
 let
@@ -369,11 +512,14 @@ end
 # ╠═fefc8298-6abf-11ef-0cc9-09ed3cc4c051
 # ╠═9b998e37-49e9-427d-963a-c8beb6e2c58b
 # ╠═93894285-d47c-49e5-9519-29ecceca4f1d
-# ╠═6831726a-e050-4a13-afbc-bb1239ad2ab6
+# ╠═b018bc38-bde2-4bff-bbc8-79e4dd81938e
+# ╠═61dec821-a308-48cb-ab41-7124369810f4
 # ╠═14e78184-fa18-4489-89d8-41dfd3044203
 # ╠═213362ac-4d9e-4dfa-b9da-b9eb23c12a29
 # ╠═a11a50ae-5aff-49aa-94e7-7f9fd688efb4
 # ╠═186b86f8-4eba-4b45-9ca4-8d2a1d8e90cb
+# ╠═d0cbfe44-5f88-4833-9313-bad04f51342b
 # ╠═032842c4-507b-4a58-b860-5a085b93ac47
+# ╠═2d44b2a8-218b-49dd-ac6d-f4ac45e2c216
 # ╠═e73d8171-98df-4955-a627-20b40a9a2a5c
 # ╠═a3c71e4e-03ca-41fd-8a1d-6cedd0da068e
