3107e1e3044e0c01696df8906e85d718045cf9e1
yanivabir <yanivabir@gmail.com>
2024-09-19 10:57:39 +0100

    pilot 2 management code

--- experiment_management/pilot2.jl
+++ experiment_management/pilot2.jl
@@ -14,11 +14,72 @@ begin
     Pkg.instantiate()
 	using Random, DataFrames, JSON, CSV, StatsBase, JLD2, HTTP
 	include("fetch_preprocess_data.jl")
+	nothing
 end
 
 # ╔═╡ 6eba46dc-855c-47ca-8fa9-8405b9566809
-jspsych_data, records = get_REDCap_data("pilot2"; file_field = "file_data")
+jspsych_data = let
+	
+	jspsych_json, records = get_REDCap_data("pilot2"; file_field = "file_data")
+	
+	jspsych_data = REDCap_data_to_df(jspsych_json, records)
+end
+
+# ╔═╡ f47e6aba-00ea-460d-8310-5b24ed7fe336
+function extract_debrief_responses(data::DataFrame)
+	# Select trials
+	debrief = filter(x -> !ismissing(x.trialphase) && x.trialphase in ["debrief_text", "debrief_likert"], data)
+
+	# Select variables
+	select!(debrief, [:prolific_pid, :exp_start_time, :trialphase, :response])
+
+	# Long to wide
+	debrief = unstack(
+		debrief,
+		[:prolific_pid, :exp_start_time],
+		:trialphase,
+		:response
+	)
+
+	# Parse JSON and make into DataFrame
+	likert_expanded = 
+		DataFrame([JSON.parse(row.debrief_likert) for row in eachrow(debrief)])
+
+	text_expanded = 
+		DataFrame([JSON.parse(row.debrief_text) for row in eachrow(debrief)])
+
+	# hcat together
+	return hcat(debrief[!, Not([:debrief_likert, :debrief_text])], likert_expanded, text_expanded)
+end
+
+# ╔═╡ d203faab-d4ea-41b2-985b-33eb8397eecc
+function summarize_participation(data::DataFrame)
+	participants = combine(groupby(data, [:prolific_pid, :record_id, :exp_start_time]),
+		:trialphase => (x -> "experiment_end_message" in x) => :finished,
+		:trialphase => (x -> "kick-out" in x) => :kick_out,
+		:outcomes => 
+			(x -> filter(y -> !ismissing(y), unique(x)) |> z -> length(z) > 0 ? z[1] : missing) => :PILT_bonus,
+		:vigour_bonus => (x -> all(ismissing.(x)) ? missing : (filter(y -> !ismissing(y), unique(x))[1])) => :vigour_bonus,
+		[:trial_type, :block] => ((t, b) -> sum((t .== "PLT") .& (typeof.(b) .== Int64))) => :n_trial_PLT,
+		:total_presses => (x -> length(filter(y -> !ismissing(y), x))) => :n_trial_vigour,
+		:block => (x -> filter(y -> typeof(y) == Int64, x) |> unique |> length) => :n_blocks_PLT,
+		:n_warnings => maximum => :n_warnings
+	)
+
+	debrief = extract_debrief_responses(data)
+
+	participants = leftjoin(participants, debrief, 
+		on = [:prolific_pid, :exp_start_time])
+
+	return participants
+end
+
+# ╔═╡ e35effd6-5c62-48aa-8932-872c7af50d7b
+summarize_participation(jspsych_data)
 
 # ╔═╡ Cell order:
 # ╠═da2aa306-75f9-11ef-2592-2be549c73d82
 # ╠═6eba46dc-855c-47ca-8fa9-8405b9566809
+# ╠═e35effd6-5c62-48aa-8932-872c7af50d7b
+# ╠═d203faab-d4ea-41b2-985b-33eb8397eecc
+# ╠═f47e6aba-00ea-460d-8310-5b24ed7fe336
